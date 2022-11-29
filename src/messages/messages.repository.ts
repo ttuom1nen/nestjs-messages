b1e5d7fdcc3e9348b1ee189578cdb43a76334405
ttuom1nen <ttuominen@outlook.com>
2022-11-29 12:22:36 +0200

    Implement messages repository

---
+++ src/messages/messages.repository.ts
@@ -0,0 +1,31 @@
+import { randomBytes } from 'crypto';
+import { readFile, writeFile } from 'fs/promises';
+
+export class MessagesRepository {
+  async findOne(id: string) {
+    const contents = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(contents);
+
+    return messages[id];
+  }
+
+  async findAll() {
+    const contents = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(contents);
+
+    return messages;
+  }
+
+  async create(content: string) {
+    const contents = await readFile('messages.json', 'utf8');
+    const messages = JSON.parse(contents);
+
+    const id = randomBytes(20).toString('hex');
+    const message = { id, content };
+    messages[id] = message;
+
+    await writeFile('messages.json', JSON.stringify(messages));
+
+    return message;
+  }
+}
